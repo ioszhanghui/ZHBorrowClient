b5eb330838e90f411e02da1dcbf5d082411c9485
huihui <@ioszhanghui@163.com>
2018-03-09 15:00:50 +0800

    我全部删除了

--- ZHBorrowClient/Class/Home(首页)/View/FetchObj.m
+++ ZHBorrowClient/Class/Home(首页)/View/FetchObj.m
@@ -10,22 +10,4 @@
 
 @implementation FetchObj
 
-+(void)huihuiBranch{
-    
-    NSLog(@"我要提交到huihui分支");
-    NSLog(@"huihuiBranch23提交到huihui分支");
-}
-
-+(void)huihuiBranch234{
-    
-    NSLog(@"huihuiBranch23提交到huihui分支");
-    NSLog(@"huihuiBranch234提交到huihui分支");
-}
-+(void)huihuiBranch235{
-    
-    NSLog(@"huihuiBranch23提交到huihui分支");
-    NSLog(@"huihuiBranch235提交到huihui分支");
-}
-
-
 @end
